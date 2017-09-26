e383a64810840ed20be7bb0f31f98c3fa2b011ed
sarahkemi <sarahaladetan@gmail.com>
2017-09-26 16:30:16 -0400

    loaded data onto page

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+import json 
+
+data = {}
+with open('data.txt') as json_file:  
+    data = json.load(json_file)
+
+print(data['1793'])
+
 app = Flask(__name__)
 
 @app.route('/')
 def render():
-    return render_template('myfile.html')
+    return render_template('myfile.html',data=data)
 
 if __name__=='__main__':
     app.run()
